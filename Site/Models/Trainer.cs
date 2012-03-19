9c8290acccac8e26b2985ac38989113551ef3623
Michael Barber <bigmichael_12@hotmail.com>
2012-03-19 23:14:04 +1000

    Removed Sencha and added a few models

---
+++ Site/Models/Trainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Site.Models
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+
+    }
+
+    public class TrainingSession
+    {
+        public DateTime SessionStart { get; set; }
+        public DateTime SessionEnd { get; set; }
+        public Trainer trainer { get; set; }
+        public IEnumerable<Activity> Activities { get; set; }
+    }
+
+    public class Activity
+    {
+        public string name { get; set; }
+        public int DifficultyLevel { get; set; }
+    }
+}
